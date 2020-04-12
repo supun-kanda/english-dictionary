13aa7f0c917328955c65751c4f1029a91acf9e3b
supun-kanda <supunkandambige9@gmail.com>
2020-04-12 16:18:13 +0530

    card-35788745 separate bff and fe servers initiated

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/web-app/index.js',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `${__dirname}/src/web-app/public/index.html`,
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    devServer: {
+        writeToDisk: true,
+        historyApiFallback: true,
+        contentBase: './dist',
+        port: 8080
+    }
+}
